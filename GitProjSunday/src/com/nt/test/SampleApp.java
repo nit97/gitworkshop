e8ea74f7edf4495ac52c935adb2ad095a80af659
janiDev1 <janiDev1@gmail.com>
2018-06-26 20:10:48 +0530

    change Project

--- GitProjSunday/src/com/nt/test/SampleApp.java
+++ GitProjSunday/src/com/nt/test/SampleApp.java
@@ -5,6 +5,10 @@ public class SampleApp
 	public static void main(String[] args) 
 	{
 		System.out.println("SampleAPP");
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(i);
+		}
 		
 
 	}//main
